c6920259cc664b130d34da37a83ae12edd325712
Irene Martorelli <54712145+imartorelli@users.noreply.github.com>
2019-09-06 18:05:41 +0200

    Add files via upload

---
+++ ncbi_data_acquisition/conf_lit.py
@@ -0,0 +1,20 @@
+ncbi_access1 = {
+  "account": "<ncbi_account>",
+  "name": "<user>"
+}
+
+ncbi_pmid_request = {
+"ncbi_pmid_api_converter": "https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids=",
+"ncbi_tool_path": "&tool=my_tool&email=",
+"ncbi_pmid_path": "https://www.ncbi.nlm.nih.gov/pubmed/?term="	
+}
+
+ebi_pmid_request = {
+"ebi_api_path": "https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=",
+"ebi_xml_format": "&format=xml"	
+}
+
+sra_request = {
+"url_bioproject": "http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term=",
+"url_sra": "http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term="
+}
